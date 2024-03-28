1452f38206708a78c8856988c1c1a84673fa4f1d
Manogya <manogya57@gmail.com>
2024-03-28 22:30:43 +0545

    changed background image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,22 +1,21 @@
 import {Fragment} from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.png";
+import classes from "./Header.module.css";
 
-
-const Header = props => {
-
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>React Meals</h1>
-    <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
-        </header>
-        <div className={classes['main-image']}>
-        <img src={mealsImage} alt="Food Background"/>
-
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>MoMo Pasal</h1>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Food Background" />
+      </div>
     </Fragment>
-}
+  );
+};
 
 export default Header;
